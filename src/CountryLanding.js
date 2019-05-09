d99a471730bdc15b5f5e8422a3c4bf3e5b1e3437
kunalk <toml.mob2016@gmail.com>
2019-05-09 17:05:02 +0530

    cal

--- src/CountryLanding.js
+++ src/CountryLanding.js
@@ -1,19 +1,111 @@
+import './styles.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import { CHANGE_COUNTRY, CLOSE, DASH_LOGO } from './assets';
+
+import Button from '@material-ui/core/Button';
+import { Carousel } from 'react-responsive-carousel';
+import Dialog from '@material-ui/core/Dialog';
+import ExchangeCalculator from './ExchangeCalculator';
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 class CountryLanding extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = { country: {}, showDialog: true }
     }
 
-    componentDidMount()
-    {
+    componentDidMount() {
         console.log(this.props.location.state)
+        this.setState({ country: this.props.location.state })
     }
     render() {
         return (
-            <div>
+            <div style={{ flex: 1, flexDirection: 'column', width: '100%' }}>
+                <div style={{
+                    display: 'flex', width: '100%',
+                    justifyContent: 'space-between',
+                    height: 50, background: '#20bad2',
+                }}>
+                    <img alt="img" style={{
+                        width: 130, height: 40,
+                        backgroundSize: 'contain',
+                        marginLeft: 10, marginTop: 5
+                    }} src={DASH_LOGO}></img>
 
+                    <div
+                        style={{
+                            float: 'right',
+                            backgroundSize: 'contain',
+                            backgroundRepeat: 'no-repeat',
+                            paddingLeft: 20,
+                            paddingTop: 5,
+                            minWidth: 100,
+                            textAlign: 'center',
+                            paddingRight: 38,
+                            backgroundImage: 'url(' + CHANGE_COUNTRY + ')'
+                        }}>
+                        <span style={{ fontSize: 18, marginTop: 5 }}>{this.state.country.name}</span>
+                    </div>
+                </div>
+
+                <div style={{ paddingTop: 20, paddingBottom: 20, background: '#20bad2', display: 'flex' }}>
+
+                    <Carousel
+
+                        showArrows={false}
+                        showIndicators={false}
+                        showThumbs={false}
+                        showStatus={false}>
+                        <img alt="img1" style={{ width: '100%', height: '100%' }} src="http://test.remit2india.com/dynamicContent/TOML/Images/UK_01.png" />
+                        <img alt="img1" style={{ width: '100%', height: '100%' }} src="http://test.remit2india.com/dynamicContent/TOML/Images/UK_02.png" />
+
+                        <img alt="img1" style={{ width: '100%', height: '100%' }} src="http://test.remit2india.com/dynamicContent/TOML/Images/UK_03.png" />
+
+                    </Carousel >
+                </div>
+                <ExchangeCalculator></ExchangeCalculator>
+
+                <Dialog
+                    fullWidth
+                    maxWidth={'lg'}
+                    aria-labelledby="max-width-dialog-title"
+                    autoScrollBodyContent={false}
+                    open={this.state.showDialog}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',backgroundColor:'transparent' }}>
+                        <div style={{
+                            padding: 16, display: 'flex',
+                            width: '100%',
+                            boxSizing: 'border-box',
+                            alignItems: 'center',
+                            flexDirection: 'column', backgroundColor: '#d1d1d1'
+                        }}>
+
+                            <span>Get started with your e-mail</span>
+                            <TextField
+                                fullWidth
+                                id="standard-name"
+                                label="Email ID"
+                                margin="dense" />
+
+                            <Button style={{
+                                marginTop: 20,
+                                width: '100%', height: 50,
+                                borderRadius: 25,
+                                alignItems: 'center', justifyContent: 'center',
+                                background: '#faa62a', display: 'flex'
+                            }}>
+                                <span style={{ fontSize: 20 }}>Submit</span>
+                            </Button>
+                        </div>
+
+                        <img alt="close" src={CLOSE} style={{
+                            width: 30,
+                            height: 30, marginTop: 30
+                        }}></img>
+                    </div>
+                </Dialog>
             </div>
         );
     }
---
+++ src/ExchangeCalculator.js
@@ -0,0 +1,112 @@
+import './styles.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import React, { Component } from 'react'
+
+import Button from '@material-ui/core/Button';
+import { EQUALS } from './assets';
+
+export default class ExchangeCalculator extends Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = { currentTab: 'indicative' }
+  }
+
+  render() {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', width: 'auto', alignItems: 'center', padding: 16 }}>
+
+        <span style={{ fontSize: 22, margin: 12 }}>Today's Exchange Rate</span>
+
+        <div style={{ display: 'flex', width: '100%', height: 50 }}>
+
+          <div style={{
+            display: 'flex',
+            flex: 1, width: '100%', height: '100%',
+            position: 'relative',
+            justifyContent: 'center', alignItems: 'center', flexDirection: 'column'
+          }} className={this.state.currentTab === 'indicative' ? 'activeTab' : 'inactiveTab'} onClick={() => this.setState({ currentTab: 'indicative' })}>
+            <span style={{ fontSize: 14 }}>Indicative Rate</span>
+            <span style={{ fontSize: 18 }}>INR 90.00</span>
+
+            <div style={{
+              width: '100%', background: this.state.currentTab === 'indicative' ? '#1074ac' : '#c5ecff',
+              height: 2, position: 'absolute', bottom: 0
+            }}></div>
+
+          </div>
+
+          <div style={{
+            display: 'flex',
+            flex: 1, width: '100%', height: '100%',
+            position: 'relative',
+            justifyContent: 'center', alignItems: 'center', flexDirection: 'column'
+          }} className={this.state.currentTab === 'guranteed' ? 'activeTab' : 'inactiveTab'} onClick={() => this.setState({ currentTab: 'guranteed' })}>
+            <span style={{ fontSize: 14 }}>Guranteed Rate</span>
+            <span style={{ fontSize: 18 }}>INR 90.00</span>
+
+            <div style={{
+              width: '100%', background: this.state.currentTab === 'guranteed' ? '#1074ac' : '#c5ecff',
+              height: 2, position: 'absolute', bottom: 0
+            }}></div>
+
+          </div>
+
+        </div>
+        <span style={{ fontSize: 16, margin: 18 }}>Calculate the amount you wish to send</span>
+
+        <div style={{
+          display: 'flex', width: '100%', justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+
+          <div
+            style={{ display: 'flex', flex: 1, height: 60, padding: 8 }}>
+
+            <div
+              style={{ display: 'flex', width: '100%', flexDirection: 'column' }}>
+              <span style={{ fontSize: 12 }}>Sending Amount</span>
+
+              <div style={{ display: 'flex', height: 30, alignItems: 'flex-end', marginTop: 10, }}>
+                <span style={{ fontSize: 16, marginBottom: 2 }}>USD</span>
+                <input type="number" style={{ marginLeft: 4, width: '100%', border: 'none', fontSize: 20 }}></input>
+              </div>
+              <div style={{ width: '100%', height: 2, marginTop: 2, background: '#20bad2' }}></div>
+            </div>
+
+          </div>
+          <img alt="eq"
+            style={{ width: 40, height: 40 }}
+            src={EQUALS}></img>
+
+          <div
+            style={{ display: 'flex', flex: 1, height: 60, padding: 8 }}>
+
+            <div
+              style={{ display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'flex-end' }}>
+              <span style={{ fontSize: 12 }}>Receiving Amount</span>
+
+              <div style={{ display: 'flex', height: 30, marginTop: 10, alignItems: 'flex-end' }}>
+                <input type="number" style={{ marginLeft: 4, width: '100%', border: 'none', fontSize: 20 }}></input>
+                <span style={{ fontSize: 16, marginBottom: 2 }}>USD</span>
+              </div>
+              <div style={{ width: '100%', height: 2, marginTop: 2, background: '#20bad2' }}></div>
+            </div>
+          </div>
+        </div>
+
+        <Button style={{
+          marginTop: 30,
+          width: '100%', height: 50,
+          borderRadius: 25,
+          alignItems: 'center', justifyContent: 'center',
+          background: '#faa62a', display: 'flex'
+        }}>
+          <span style={{fontSize:20}}>Submit</span>
+        </Button>
+      </div>
+    )
+  }
+}
