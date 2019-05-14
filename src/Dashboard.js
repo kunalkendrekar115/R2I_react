791c68ebaf7db26549d8da7ea866848ac3f77da6
Kunal Kendrekar <toml.mob2016@gmail.com>
2019-05-14 21:24:52 +0530

    dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -8,6 +8,8 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import Login from './Login';
 
 const styles = {
     stickToBottom: {
@@ -33,23 +35,36 @@ class Dashboard extends React.Component {
 
         return (
             <div style={{ width: '100%' }}>
+
+
                 <BottomNavigation
                     value={value}
                     onChange={this.handleChange}
                     showLabels
                     style={{ width: '100%' }}
-                    className={classes.stickToBottom}
-                >
+                    className={classes.stickToBottom}>
+
                     <BottomNavigationAction
-                        style={{ width: '33%',color:'#1074ac',fontSize:9}}
-                        label="Recents" icon={<HomeIcon />} />
+                        style={{ width: '33%', color: '#1074ac', fontSize: 9 }}
+                        icon={<HomeIcon></HomeIcon>}
+                        label="Recents" />
                     <BottomNavigationAction
-                        style={{ width: '33%',color:'#1074ac' }}
+                        style={{ width: '33%', color: '#1074ac' }}
                         label="Favorites" icon={<FavoriteIcon />} />
                     <BottomNavigationAction
-                        style={{ width: '33%',color:'#1074ac' }}
+                        style={{ width: '33%', color: '#1074ac' }}
+                        label="Nearby" icon={<LocationOnIcon />} />
+                    <BottomNavigationAction
+                        style={{ width: '33%', color: '#1074ac' }}
+                        label="Nearby" icon={<LocationOnIcon />} />
+                    <BottomNavigationAction
+                        style={{ width: '33%', color: '#1074ac' }}
                         label="Nearby" icon={<LocationOnIcon />} />
                 </BottomNavigation>
+
+                <div style={{ marginBottom: 60 }}>
+                    {this.state.value === 0 && <Login></Login>}
+                </div>
             </div>
 
         );
